3ef282447195a8f64607c4d354e888e8dfd3e883
Yvan Nollet <yvan.nollet@rayference.eu>
2021-02-19 13:52:59 +0100

    Added .gitignore file render script avatars and captions file

---
+++ render.sh
@@ -0,0 +1,1 @@
+gource -1920x1080 -r 60 --key --caption-file captions.txt --caption-size 24 --caption-duration 5 --user-image-dir avatar/ -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 eradiate.mp4
